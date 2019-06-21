cf57ae979d9e00da8651600684c16032946e76e8
seatong <48592797+seatang@users.noreply.github.com>
2019-06-21 16:06:38 +0800

    配置首页和登录页路由

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,22 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+// 配置一级路由
+import AppHome from '@/views/home'
+import AppLogin from '@/views/login'
 
 Vue.use(Router)
 
 export default new Router({
-  routes: [ ]
+  routes: [
+    {
+      name: 'home',
+      path: '/',
+      component: AppHome
+    },
+    {
+      name: 'login',
+      path: '/login',
+      component: AppLogin
+    }
+  ]
 })
